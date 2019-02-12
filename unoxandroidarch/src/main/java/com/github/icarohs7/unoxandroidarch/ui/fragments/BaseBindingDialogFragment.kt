5442f402bb064f600cf9663e8a3cffe8bbcd5f54
Icaro Temponi <icarohs7@gmail.com>
2019-02-12 10:20:47 -0200

    Improve BaseBindingDialogFragment and resources related to it
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- unoxandroidarch/src/main/java/com/github/icarohs7/unoxandroidarch/ui/fragments/BaseBindingDialogFragment.kt
+++ unoxandroidarch/src/main/java/com/github/icarohs7/unoxandroidarch/ui/fragments/BaseBindingDialogFragment.kt
@@ -9,8 +9,11 @@ import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.DialogFragment
 import com.github.icarohs7.unoxandroid.extensions.coroutines.cancelCoroutineScope
+import com.github.icarohs7.unoxandroidarch.R
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
+import org.jetbrains.anko.matchParent
+
 
 /**
  * Base implementation of a dialog fragment
@@ -23,6 +26,11 @@ abstract class BaseBindingDialogFragment<DB : ViewDataBinding> : DialogFragment(
      */
     lateinit var binding: DB
         private set
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.AppTheme_FullScreenDialog)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil
@@ -44,6 +52,15 @@ abstract class BaseBindingDialogFragment<DB : ViewDataBinding> : DialogFragment(
      * Called after [onBindingCreated]
      */
     open fun afterInitialSetup() {
+    }
+
+    /**
+     * Make the dialog fullscreen on start
+     */
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(matchParent, matchParent)
+        dialog?.window?.setWindowAnimations(R.style.AppTheme_Slide)
     }
 
     /**

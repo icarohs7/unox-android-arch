18445c0f4945bc928766c1ef6ba422ac5a90fd0d
Icaro Temponi <icarohs7@gmail.com>
2019-02-21 15:21:51 -0300

    Add functionalities
    Update dependencies
    
    Add combineRepositoryFlowables accepting from 2 to 6 repositories
    
    Rename setImageDrawable extension of ImageView to load
    
    Add load extension on ImageView used to load an image from an url
    
    Add top level function createRetrofitService used to create an instance
    of Retrofit with default converters and interceptors
    
    Add init function to the companion object of the UnoxAndroidArch
    interface, used to initialize the library and add some of its
    features to the DI container
    
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- unoxandroidarch/src/main/java/com/github/icarohs7/unoxandroidarch/extensions/ImageViewExtensions.kt
+++ unoxandroidarch/src/main/java/com/github/icarohs7/unoxandroidarch/extensions/ImageViewExtensions.kt
@@ -2,10 +2,30 @@ package com.github.icarohs7.unoxandroidarch.extensions
 
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
+import com.squareup.picasso.Picasso
+import com.squareup.picasso.RequestCreator
 
 /**
- * Load a image drawable onto a ImageView from a drawable resource
+ * Load an image drawable onto an ImageView from a drawable resource
  */
-fun ImageView.setImageDrawable(@DrawableRes resource: Int) {
+fun ImageView.load(@DrawableRes resource: Int) {
     setImageDrawable(resource.drawableByResourceId(context))
+}
+
+/**
+ * Load an image at the given url to the
+ * receiving ImageView
+ */
+fun ImageView.load(
+        url: String,
+        @DrawableRes placeholder: Int? = null,
+        @DrawableRes onError: Int? = null,
+        additionalSetup: RequestCreator.() -> RequestCreator = { this }
+) {
+    Picasso.get()
+            .load(url)
+            .apply { placeholder?.let(::placeholder) }
+            .apply { onError?.let(::error) }
+            .additionalSetup()
+            .into(this)
 }

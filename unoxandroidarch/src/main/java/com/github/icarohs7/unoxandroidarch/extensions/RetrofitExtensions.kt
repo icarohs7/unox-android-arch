bd649d4e8d7341fc5f81124b836aaad1531a38df
Icaro Temponi <icarohs7@gmail.com>
2019-02-27 09:48:48 -0300

    Update dependencies, update createRetrofitService top level function
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- unoxandroidarch/src/main/java/com/github/icarohs7/unoxandroidarch/extensions/RetrofitExtensions.kt
+++ unoxandroidarch/src/main/java/com/github/icarohs7/unoxandroidarch/extensions/RetrofitExtensions.kt
@@ -1,11 +1,12 @@
 package com.github.icarohs7.unoxandroidarch.extensions
 
 import com.facebook.stetho.okhttp3.StethoInterceptor
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.serializationConverterFactory
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
 import retrofit2.Retrofit
 import timber.log.Timber
 
@@ -15,17 +16,34 @@ import timber.log.Timber
  * an instance of a given service
  */
 inline fun <reified T> createRetrofitService(baseUrl: String): T {
-    val contentType = MediaType.get("application/json")
-    val jsonAdapter = Json.nonstrict
-    val serializationConverter = serializationConverterFactory(contentType, jsonAdapter)
-    val interceptor = HttpLoggingInterceptor { Timber.tag("Retrofit").d(it) }
-            .apply { level = HttpLoggingInterceptor.Level.BODY }
-    val client = OkHttpClient.Builder().addInterceptor(interceptor).addInterceptor(StethoInterceptor()).build()
-    val retrofit = Retrofit
+    return Retrofit
             .Builder()
             .baseUrl(baseUrl)
-            .addConverterFactory(serializationConverter)
-            .client(client)
+            .addConverterFactory(RetrofitExtensions.getJsonConverter())
+            .client(RetrofitExtensions.getHttpClient())
             .build()
-    return retrofit.create(T::class.java)
+            .create(T::class.java)
+}
+
+object RetrofitExtensions {
+    fun getJsonConverter(): Converter.Factory {
+        val contentType = MediaType.get("application/json")
+        return Json
+                .nonstrict
+                .asConverterFactory(contentType)
+    }
+
+    fun getHttpClient(): OkHttpClient {
+        return OkHttpClient
+                .Builder()
+                .addInterceptor(getInterceptor())
+                .addInterceptor(StethoInterceptor())
+                .build()
+    }
+
+    private fun getInterceptor(): HttpLoggingInterceptor {
+        val log = { s: String -> Timber.tag("Retrofit").d(s) }
+        return HttpLoggingInterceptor(log)
+                .apply { level = HttpLoggingInterceptor.Level.BODY }
+    }
 }

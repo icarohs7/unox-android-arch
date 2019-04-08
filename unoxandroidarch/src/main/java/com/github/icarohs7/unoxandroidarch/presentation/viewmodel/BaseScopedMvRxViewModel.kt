5cf83f2be093cb58ea9a033a5809e8efbf24a699
Icaro Temponi <icarohs7@gmail.com>
2019-04-08 10:09:59 -0300

    Make BaseScopedMvRxViewModel abstract
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- unoxandroidarch/src/main/java/com/github/icarohs7/unoxandroidarch/presentation/viewmodel/BaseScopedMvRxViewModel.kt
+++ unoxandroidarch/src/main/java/com/github/icarohs7/unoxandroidarch/presentation/viewmodel/BaseScopedMvRxViewModel.kt
@@ -2,15 +2,18 @@ package com.github.icarohs7.unoxandroidarch.presentation.viewmodel
 
 import com.airbnb.mvrx.BaseMvRxViewModel
 import com.airbnb.mvrx.MvRxState
+import com.airbnb.mvrx.MvRxStateStore
+import com.airbnb.mvrx.RealMvRxStateStore
 import com.github.icarohs7.unoxandroidarch.UnoxAndroidArch
 import com.github.icarohs7.unoxcore.extensions.coroutines.cancelCoroutineScope
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
 
-class BaseScopedMvRxViewModel<S : MvRxState>(
+abstract class BaseScopedMvRxViewModel<S : MvRxState>(
         initialState: S,
-        debugMode: Boolean = UnoxAndroidArch.isDebug
-) : BaseMvRxViewModel<S>(initialState, debugMode), CoroutineScope by MainScope() {
+        debugMode: Boolean = UnoxAndroidArch.isDebug,
+        stateStore: MvRxStateStore<S> = RealMvRxStateStore(initialState)
+) : BaseMvRxViewModel<S>(initialState, debugMode, stateStore), CoroutineScope by MainScope() {
     override fun onCleared() {
         cancelCoroutineScope()
         super.onCleared()

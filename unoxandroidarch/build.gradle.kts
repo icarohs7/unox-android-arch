492a60f189e1f0e2838b61c8ba12d7dcc3e1f334
Icaro Temponi <icarohs7@gmail.com>
2019-04-23 13:36:12 -0300

    Use kotlin multiplatform to setup the project, add bintray publication
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- unoxandroidarch/build.gradle.kts
+++ unoxandroidarch/build.gradle.kts
@@ -1,74 +1,100 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
+    kotlin("multiplatform")
+    kotlin("kapt")
     id("com.android.library")
-    id("kotlin-android")
-    id("kotlin-kapt")
     id("kotlinx-serialization")
     id("androidx.navigation.safeargs.kotlin")
-    id("com.github.dcendents.android-maven") version "2.1"
     id("jacoco")
+    id("maven-publish")
+    id("com.jfrog.bintray")
+    id("com.github.b3er.local.properties") version "1.1"
     defaults.`android-module`
 }
 
 android {
     defaultSettings()
-    defaultConfig {
-        versionCode = 1
-        versionName = "1.0"
+}
+
+kotlin {
+    metadata { mavenPublication { artifactId = "unox-android-arch-metadata" } }
+
+    android {
+        mavenPublication { artifactId = "unox-android-arch" }
+        publishLibraryVariants("debug")
+        compilations.all { kotlinOptions.jvmTarget = "1.6" }
+    }
+
+    @Suppress("UNUSED_VARIABLE")
+    sourceSets {
+        val androidMain by getting {
+            kotlin.srcDir("src/main/kotlin")
+            dependencies {
+                compileOnly("javax.annotation:javax.annotation-api:1.3.2")
+
+                api(Deps.coroutinesRx2)
+                api(Deps.khronos)
+                api(Deps.kotlinxSerialization)
+                api(Deps.okHttp)
+                api(Deps.okHttpLoggingInterceptor)
+                api(Deps.retrofit)
+                api(Deps.retrofitKotlinxSerializationConverter)
+                api(Deps.rxRelay)
+
+                api(AndroidDeps.ankoSdk25)
+                api(AndroidDeps.appUpdater)
+                api(AndroidDeps.bungee)
+                api(AndroidDeps.constraintLayout)
+                api(AndroidDeps.drawableToolbox)
+                api(AndroidDeps.flashbar)
+                api(AndroidDeps.fragmentKtx)
+                api(AndroidDeps.koinAndroid)
+                api(AndroidDeps.lifecycleReactiveStreamsKtx)
+                api(AndroidDeps.materialDesign)
+                api(AndroidDeps.materialDialogs)
+                api(AndroidDeps.materialSpinner)
+                api(AndroidDeps.mvRx)
+                api(AndroidDeps.picasso)
+                api(AndroidDeps.pugNotification)
+                api(AndroidDeps.quantum)
+                api(AndroidDeps.quantumRx)
+                api(AndroidDeps.reactiveNetwork)
+                api(AndroidDeps.recyclerView)
+                api(AndroidDeps.roomRuntime)
+                api(AndroidDeps.roomRxJava2)
+                api(AndroidDeps.rxPermission)
+                api(AndroidDeps.smartScheduler)
+                api(AndroidDeps.spinKit)
+                api(AndroidDeps.splittiesAppctx)
+                api(AndroidDeps.splittiesResources)
+                api(AndroidDeps.spotsDialog)
+                api(AndroidDeps.stateViews)
+                api(AndroidDeps.stetho)
+                api(AndroidDeps.stethoOkHttp)
+                api(AndroidDeps.timber)
+                api(AndroidDeps.unoxCoreAndroid)
+            }
+        }
+
+        val androidTest by getting {
+            dependsOn(androidMain)
+            kotlin.srcDir("src/test/kotlin")
+            dependencies {
+                TestDeps.androidCore.forEach {
+                    implementation(it) {
+                        exclude(group = "org.apache.maven")
+                    }
+                }
+            }
+        }
     }
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.6"
-}
-
 dependencies {
-    api(Deps.coroutinesRx2)
-    api(Deps.khronos)
-    api(Deps.kotlinxSerialization)
-    api(Deps.okHttp)
-    api(Deps.okHttpLoggingInterceptor)
-    api(Deps.retrofit)
-    api(Deps.retrofitKotlinxSerializationConverter)
-    api(Deps.rxRelay)
-
-    api(AndroidDeps.ankoSdk25)
-    api(AndroidDeps.appUpdater)
-    api(AndroidDeps.bungee)
-    api(AndroidDeps.constraintLayout)
-    api(AndroidDeps.drawableToolbox)
-    api(AndroidDeps.flashbar)
-    api(AndroidDeps.fragmentKtx)
-    api(AndroidDeps.koinAndroid)
-    api(AndroidDeps.lifecycleReactiveStreamsKtx)
-    api(AndroidDeps.materialDesign)
-    api(AndroidDeps.materialDialogs)
-    api(AndroidDeps.materialSpinner)
-    api(AndroidDeps.mvRx)
-    api(AndroidDeps.picasso)
-    api(AndroidDeps.pugNotification)
-    api(AndroidDeps.quantum)
-    api(AndroidDeps.quantumRx)
-    api(AndroidDeps.reactiveNetwork)
-    api(AndroidDeps.recyclerView)
-    api(AndroidDeps.roomRuntime)
-    api(AndroidDeps.roomRxJava2)
-    api(AndroidDeps.rxPermission)
-    api(AndroidDeps.smartScheduler)
-    api(AndroidDeps.spinKit)
-    api(AndroidDeps.splittiesAppctx)
-    api(AndroidDeps.splittiesResources)
-    api(AndroidDeps.spotsDialog)
-    api(AndroidDeps.stateViews)
-    api(AndroidDeps.stetho)
-    api(AndroidDeps.stethoOkHttp)
-    api(AndroidDeps.timber)
-    api(AndroidDeps.unoxCoreAndroid)
-
-    AndroidKaptDeps.core.forEach(::kapt)
-    AndroidKaptDeps.core.forEach(::kaptTest)
+    AndroidKaptDeps.core.forEach { "kapt"(it) }
+    AndroidKaptDeps.core.forEach { "kaptTest"(it) }
 }
+
+setupBintrayPublish(bintray, "metadata", "androidDebug")
 
 setupJacoco {
     sourceDirectories.setFrom(files(

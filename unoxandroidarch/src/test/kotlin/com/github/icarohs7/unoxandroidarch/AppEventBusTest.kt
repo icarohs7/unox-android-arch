7819e8de9687f6752aa0ee7b7e5729b96e82f322
Icaro Temponi <icarohs7@gmail.com>
2019-04-27 20:09:40 -0300

    Use suspensions on AppEventBusTest
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- unoxandroidarch/src/test/kotlin/com/github/icarohs7/unoxandroidarch/AppEventBusTest.kt
+++ unoxandroidarch/src/test/kotlin/com/github/icarohs7/unoxandroidarch/AppEventBusTest.kt
@@ -3,7 +3,10 @@ package com.github.icarohs7.unoxandroidarch
 import android.app.Activity
 import com.github.icarohs7.unoxandroidarch.presentation.activities.BaseScopedActivity
 import com.github.icarohs7.unoxcore.extensions.addOnDestroyObserver
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -11,6 +14,8 @@ import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 import se.lovef.assert.v1.shouldBe
 import se.lovef.assert.v1.shouldEqual
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 @RunWith(RobolectricTestRunner::class)
 @Config(application = TestApplication::class)
@@ -22,32 +27,43 @@ class AppEventBusTest {
 
         var v: Activity? = null
         onActivity { v = this@onActivity }
-        delay(400)
         v shouldEqual act
         v shouldBe act
 
         onActivity<BaseScopedActivity> { v = null }
-        delay(400)
         v shouldEqual null
         v shouldBe null
 
         onActivity<TestActivity> { v = this@onActivity }
-        delay(400)
         v shouldEqual act
         v shouldBe act
 
         onActivity<Activity> { v = null }
-        delay(400)
         v shouldEqual null
         v shouldBe null
 
-        var v1 = 0
-        act.addOnDestroyObserver { v1 = 1 }
+        val async1 = async {
+            suspendCoroutine<Int> { continuation ->
+                act.addOnDestroyObserver { continuation.resume(1532) }
+            }
+        }
+        delay(200)
+
+        controller.stop()
         controller.destroy()
-        delay(800)
-        onActivity<TestActivity> { v = this@onActivity }
-        v shouldEqual null
-        v shouldBe null
-        v1 shouldEqual 1
+        delay(400)
+
+        val async2 = async(start = CoroutineStart.LAZY) {
+            suspendCoroutine<Int> { continuation ->
+                onActivity<TestActivity> { continuation.resume(42) }
+                launch {
+                    delay(2000)
+                    continuation.resume(17)
+                }
+            }
+        }
+
+        async1.await() shouldEqual 1532
+        async2.await() shouldEqual 17
     }
 }

09dea2c39e7550a14372b133ba8e92ec6f41a27a
Icaro Temponi <icarohs7@gmail.com>
2019-05-23 10:18:57 -0300

    Add top level function, change location acquiring strategy for function
    Add getLocationFromProvider top level function
    
    Change getCurrentLocation location acquiring strategy
    
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- unoxandroidarch/src/main/kotlin/com/github/icarohs7/unoxandroidarch/toplevel/Location.kt
+++ unoxandroidarch/src/main/kotlin/com/github/icarohs7/unoxandroidarch/toplevel/Location.kt
@@ -1,45 +1,48 @@
 package com.github.icarohs7.unoxandroidarch.toplevel
 
-import android.annotation.SuppressLint
+import android.Manifest
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
+import androidx.annotation.RequiresPermission
 import arrow.core.Try
 import splitties.systemservices.locationManager
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.resume
-import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
 /**
- * Return the last know location if available or request the
- * current location using the network provider. **Needs permission**
+ * Try to get the current location using the given means by default:
+ * - Try to request the current location using GPS
+ * - Try to request the current location using the Network
+ * - Try to get the last known location acquired through GPS
+ * - Try to get the last known location acquired through the Network
  */
-@SuppressLint("MissingPermission")
+@RequiresPermission(anyOf = [Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION])
 suspend fun getCurrentLocation(): Try<Location> {
     return Try {
-        val locManager = locationManager
-        val lastGpsLoc: Location? = locManager.getLastKnownLocation(
-                LocationManager.GPS_PROVIDER)
-        val lastNetworkLoc: Location? = locManager.getLastKnownLocation(
-                LocationManager.NETWORK_PROVIDER)
+        safeRun { getLocationFromProvider(LocationManager.GPS_PROVIDER) }
+                ?: safeRun { getLocationFromProvider(LocationManager.NETWORK_PROVIDER) }
+                ?: safeRun { locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER) }
+                ?: safeRun { locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER) }!!
+    }
+}
 
-        suspend fun getLoc(provider: String): Location {
-            return suspendCoroutine { continuation ->
-                val locationListener = getLocationListener(continuation)
-                locManager.requestSingleUpdate(provider, locationListener, null)
-            }
+/**
+ * Request the location from the given provider
+ * ### Requires either [android.Manifest.permission.ACCESS_COARSE_LOCATION] or [android.Manifest.permission.ACCESS_FINE_LOCATION]
+ */
+@RequiresPermission(anyOf = [Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION])
+suspend fun getLocationFromProvider(provider: String): Location? {
+    return suspendCoroutine { continuation ->
+        if (!locationManager.isProviderEnabled(provider)) {
+            continuation.resume(null)
+            return@suspendCoroutine
         }
 
-        safeRun { lastGpsLoc }
-                ?: safeRun { lastNetworkLoc }
-                ?: safeRun {
-                    getLoc(LocationManager.GPS_PROVIDER)
-                }
-                ?: safeRun {
-                    getLoc(LocationManager.NETWORK_PROVIDER)
-                }!!
+        val listener = getLocationListener(continuation)
+        locationManager.requestSingleUpdate(provider, listener, null)
     }
 }
 
@@ -57,8 +60,6 @@ private fun getLocationListener(continuation: Continuation<Location>): LocationL
 
         override fun onProviderEnabled(provider: String) {}
 
-        override fun onProviderDisabled(provider: String) {
-            continuation.resumeWithException(UnsupportedOperationException("Provider disabled"))
-        }
+        override fun onProviderDisabled(provider: String) {}
     }
 }

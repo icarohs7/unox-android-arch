c00c9a8909e995ff99c2cf4ad05bba3b3c1db76e
Icaro Temponi <icarohs7@gmail.com>
2019-05-23 09:33:10 -0300

    Remove hideKeyboard extension of Activity
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- unoxandroidarch/src/main/kotlin/com/github/icarohs7/unoxandroidarch/extensions/Activity.kt
+++ unoxandroidarch/src/main/kotlin/com/github/icarohs7/unoxandroidarch/extensions/Activity.kt
@@ -1,32 +1,12 @@
 package com.github.icarohs7.unoxandroidarch.extensions
 
 import android.Manifest
-import android.app.Activity
 import android.content.Context
-import android.view.View
-import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import com.github.icarohs7.unoxandroidarch.toplevel.hasPermissions
 import com.github.icarohs7.unoxandroidarch.toplevel.requestPermissionsInternal
-import splitties.systemservices.inputMethodManager
-
-/**
- * Dismiss the soft keyboard
- * @param containerId The id of the container within the keyboard is being shown
- */
-fun Activity.hideKeyboard(@IdRes containerId: Int) {
-    inputMethodManager.hideSoftInputFromWindow(findViewById<View>(containerId)?.windowToken, 0)
-}
-
-/**
- * Dismiss the soft keyboard
- * @param container The container within the keyboard is being shown
- */
-fun Activity.hideKeyboard(container: View) {
-    inputMethodManager.hideSoftInputFromWindow(container.windowToken, 0)
-}
 
 /** Invoke the given [observer] whenever a new fragment is attached to the supportFragmentManager */
 fun FragmentActivity.onFragmentAttached(observer: (Fragment) -> Unit) {

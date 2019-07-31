5bd856fb2873de470cdefa87d990778bcf0bc99f
Icaro Temponi <icarohs7@gmail.com>
2019-07-31 09:36:17 -0300

    Add valueOr and asFlow extensions of LiveData
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

---
+++ core/src/main/kotlin/com/github/icarohs7/unoxandroidarch/extensions/LiveDataExtensions.kt
@@ -0,0 +1,32 @@
+package com.github.icarohs7.unoxandroidarch.extensions
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import arrow.core.Try
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+
+/** Retrieve the value of the livedata or return the fallback if null */
+fun <T> LiveData<T>.valueOr(fallback: T): T =
+        value ?: fallback
+
+/**
+ * Convert the given [LiveData] to [Flow]
+ * @param emissionIfNull First emission of the Flow
+ *                      if the LiveData starts without
+ *                      a value, use null to ignore
+ *                      and start emitting only after
+ *                      the liveData is assigned for the
+ *                      first time
+ */
+fun <T> LiveData<T>.asFlow(emissionIfNull: T?): Flow<T> {
+    return callbackFlow {
+        val hasFirstEmission = emissionIfNull != null
+        if (hasFirstEmission && value == null) Try { send(emissionIfNull!!) }
+
+        val observer = Observer<T> { offer(it) }
+        observeForever(observer)
+        awaitClose { removeObserver(observer) }
+    }
+}
---
+++ core/src/test/kotlin/com/github/icarohs7/unoxandroidarch/extensions/LiveDataExtensionsKtTest.kt
@@ -0,0 +1,61 @@
+package com.github.icarohs7.unoxandroidarch.extensions
+
+import android.os.Build
+import androidx.lifecycle.MutableLiveData
+import com.github.icarohs7.unoxandroidarch.testutils.TestActivity
+import com.github.icarohs7.unoxandroidarch.testutils.TestApplication
+import com.github.icarohs7.unoxandroidarch.testutils.mockActivity
+import com.github.icarohs7.unoxcore.UnoxCore
+import com.github.icarohs7.unoxcore.extensions.coroutines.onForeground
+import com.github.icarohs7.unoxcore.extensions.valueOr
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import se.lovef.assert.v1.shouldEqual
+
+@RunWith(RobolectricTestRunner::class)
+@Config(application = TestApplication::class, sdk = [Build.VERSION_CODES.O])
+class LiveDataExtensionsKtTest {
+    @Test
+    fun should_return_livedata_value_or_fallback(): Unit = runBlocking<Unit> {
+        UnoxCore.foregroundDispatcher = Dispatchers.Main
+        val l1 = MutableLiveData<String>()
+        l1.value shouldEqual null
+        l1.valueOr("Omai wa mou shindeiru!") shouldEqual "Omai wa mou shindeiru!"
+
+        val l2 = MutableLiveData<Int>()
+        onForeground { l2.value = 42 }
+        l2.valueOr(1532) shouldEqual 42
+
+        val l3 = MutableLiveData<String>()
+        onForeground { l3.value = null }
+        l3.value shouldEqual null
+        l3.valueOr("NANI!?") shouldEqual "NANI!?"
+    }
+
+    @Test
+    fun should_convert_live_data_to_flow(): Unit = runBlockingTest {
+        val (controller, act) = mockActivity<TestActivity>()
+        controller.resume()
+        val ld1 = MutableLiveData<String>()
+        val f1 = ld1.asFlow("Ho, Mukatte Kuru no Ka?")
+
+        var last = ""
+        f1.onEach { last = it }.launchIn(act)
+        last shouldEqual "Ho, Mukatte Kuru no Ka?"
+
+        ld1.value = "Chikadzukanakya, teme o buchi nomesenainde na"
+        last shouldEqual "Chikadzukanakya, teme o buchi nomesenainde na"
+
+        //Assert cancellation
+        controller.destroy()
+        ld1.value = "NANI!?"
+        last shouldEqual "Chikadzukanakya, teme o buchi nomesenainde na"
+    }
+}
